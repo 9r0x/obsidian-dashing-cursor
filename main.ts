ab96f6e1ba4d46ce5d95b573f53186d63f411cce
Shukai Ni <nishukai@hotmail.com>
2022-08-14 21:40:17 -0700

    Initial plugin

--- main.ts
+++ main.ts
@@ -1,137 +1,104 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, Notice, MarkdownView } from "obsidian";
+type Coordinates = { x: number; y: number; getScroll: CallableFunction };
+type MarkdownSubView = { contentContainerEl: HTMLElement };
 
-// Remember to rename these classes and interfaces!
+export default class NinjaCursorPlugin extends Plugin {
+	lastPos: Coordinates = { x: 0, y: 0, getScroll: () => 0 };
+	cursorElement: HTMLSpanElement;
+	events = ["keyup", "mouseup", "touchend"];
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+	async updateCursor() {
+		const parentElement = (<MarkdownSubView>(
+			(<unknown>(
+				this.app?.workspace.getActiveViewOfType(MarkdownView)
+					?.currentMode
+			))
+		))?.contentContainerEl?.parentElement;
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+		const selection = window.getSelection();
+		if (!parentElement || !selection || !selection.focusNode) {
+			return;
+		}
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+		const focusNode = selection.focusNode;
+		const cursorRange = document.createRange();
+		if (selection.focusOffset === 0) {
+			cursorRange.setStart(focusNode, 0);
+			cursorRange.setEnd(focusNode, 1);
+		} else {
+			cursorRange.setStart(focusNode, selection.focusOffset - 1);
+			cursorRange.setEnd(focusNode, selection.focusOffset);
+		}
+
+		const cursorDOMRects = cursorRange.getClientRects();
+		//console.log(cursorDOMRects)
+		const cursorDomRect = cursorDOMRects.item(cursorDOMRects.length - 1);
+
+		if (!cursorDomRect) {
+			new Notice("Could not find cursor position");
+			return;
+		}
+
+		const lastClientX = this.lastPos.x;
+		const lastClientY = this.lastPos.y - this.lastPos.getScroll();
+		const currentClientX = selection.focusOffset
+			? cursorDomRect.right
+			: cursorDomRect.left;
+		const currentClientY = cursorDomRect.y;
+		if (lastClientX == currentClientX && lastClientY == currentClientY) {
+			return;
+		}
+
+		const dx = currentClientX - lastClientX;
+		const dy = lastClientY - currentClientY;
+		const cursorTailAngle = Math.atan2(dx, dy) + Math.PI / 2;
+		const cursorDragDistance = Math.sqrt(dx * dx + dy * dy);
+
+		this.cursorElement.style.setProperty(
+			"--cursor-drag-width",
+			`${cursorDragDistance}px`
+		);
+		this.cursorElement.style.setProperty(
+			"--cursor-drag-angle",
+			`${cursorTailAngle}rad`
+		);
+
+		this.cursorElement.style.setProperty(
+			"--cursor-height",
+			`${cursorDomRect.height}px`
+		);
+		this.cursorElement.style.setProperty("--cursor-x1", `${lastClientX}px`);
+		this.cursorElement.style.setProperty("--cursor-y1", `${lastClientY}px`);
+		this.cursorElement.style.setProperty(
+			"--cursor-x2",
+			`${currentClientX}px`
+		);
+		this.cursorElement.style.setProperty(
+			"--cursor-y2",
+			`${currentClientY}px`
+		);
+
+		/* [SN-2022-08-14] Re-animate */
+		this.cursorElement.style.animation = "none";
+		/* [SN-2022-08-14] Force reflow */
+		/* [SN-2022-08-14] REF https://stackoverflow.com/a/45036752/6604853 */
+		this.cursorElement.offsetHeight;
+		this.cursorElement.style.animation = "";
+
+		this.lastPos.x = currentClientX;
+		/* [SN-2022-08-14] Primitive would be passed by copy of value */
+		this.lastPos.getScroll = () => parentElement?.scrollTop;
+		this.lastPos.y = currentClientY + this.lastPos.getScroll();
+	}
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.cursorElement = document.body.createSpan({
+			cls: "dashing-cursor",
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.events.forEach((e: any) =>
+			this.registerDomEvent(window, e, this.updateCursor.bind(this))
+		);
 	}
 
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+	onunload() {}
 }
